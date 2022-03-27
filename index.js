ff7d07880cbc6d6e5f490a7de6d1cbb5594cf5d8
Abdul Alzenki <bojamal7@gmail.com>
2022-03-27 08:43:20 -0700

    more

--- index.js
+++ index.js
@@ -6,8 +6,6 @@
  */
 
 const BrowserEnvironment = require("jest-environment-jsdom");
-const computedStyleToInlineStyle = require("computed-style-to-inline-style");
-
 const LZUTF8 = require("lzutf8");
 // Import the functions you need from the SDKs you need
 const { initializeApp } = require("firebase/app");
@@ -71,9 +69,6 @@ class MyEnvironment extends BrowserEnvironment {
     const currentHtml = this.global.document.documentElement.outerHTML;
 
     if (currentHtml !== snapshots[snapshots.length - 1]) {
-      computedStyleToInlineStyle(this.global.document.body, {
-        recursive: true,
-      });
       snapshots.push(this.global.document.documentElement.outerHTML);
     }
   }
